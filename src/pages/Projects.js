d5b02fe55587a0578c3ff98c96138428746f784a
Ben Marshall <benleem98@yahoo.com>
2022-06-29 19:24:26 -0400

    optimizing project images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,21 +5,21 @@ const Projects = () => {
 	const projects = [
 		{
 			name: "Chirp",
-			img: "./img/social.jpg",
+			img: "./img/social.webp",
 			siteUrl: "/",
 			githubUrl: "https://github.com/benleem/chirp",
 			desc: "Chirp is a social media website built with tools like NextJs",
 		},
 		{
 			name: "Google Earth Clone",
-			img: "./img/earth.jpg",
+			img: "./img/earth.webp",
 			siteUrl: "https://goggle-earth.netlify.app/",
 			githubUrl: "https://github.com/benleem/threejs-test",
 			desc: "Example",
 		},
 		{
 			name: "Crypto Snapshot",
-			img: "./img/crypto.jpg",
+			img: "./img/crypto.webp",
 			siteUrl: "https://cryptosnapshot.netlify.app/",
 			githubUrl:
 				"https://github.com/benleem/crypto_snapshot/tree/main/cryptosnapshot",
@@ -27,7 +27,7 @@ const Projects = () => {
 		},
 		{
 			name: "Portfolio",
-			img: "./img/portfolio.jpg",
+			img: "./img/portfolio.webp",
 			siteUrl: "/",
 			githubUrl: "https://github.com/benleem/portfolio-v2",
 			desc: "Example",
